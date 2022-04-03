2f338fa593889cef305c11a4547135f9dc3e414a
lifesboy <lifesboy@yahoo.com>
2022-04-03 15:22:36 +0700

    add sh make

---
+++ make.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+method=$1
+PLUGINS=$2
+
+CURDIR=$(pwd)
+
+INSTALLEDDIR="/usr/local"
+#Dev machine
+#INSTALLEDDIR="/Volumes/Extra/workspace/selks-gpu/staging/usr/local"
+
+if ! test -n "$PLUGINS"; then
+  PLUGINS="security/clamav"
+  PLUGINS="${PLUGINS} www/c-icap"
+fi
+
+for PLGIN in ${PLUGINS}; do
+  echo "\n>>> ${PLGIN}"
+  PLGIN_SRC_DIR="${CURDIR}/${PLGIN}/src"
+  PLGIN_DIRS=$(find $PLGIN_SRC_DIR -type d)
+  PLGIN_FILES=$(find $PLGIN_SRC_DIR -type f)
+
+  if [ $method = "install" ] || [ $method = "all" ]; then
+    for DIR in ${PLGIN_DIRS}; do
+      INSTALL_DIR=${"DIR/$PLGIN_DIRS/${INSTALLEDDIR}"}
+      # mkdir -p "${INSTALL_DIR}"
+      ls -la "${INSTALL_DIR}"
+    done
+  fi
+done
