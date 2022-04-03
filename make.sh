b1ffbc9d74c5327284a11465238669feb8deca95
lifesboy <lifesboy@yahoo.com>
2022-04-03 16:15:31 +0700

    show detail of install files

--- make.sh
+++ make.sh
@@ -29,9 +29,9 @@ for PLGIN in ${PLUGINS}; do
 
     for FILE in ${PLGIN_FILES}; do
       INSTALL_FILE=$(echo $FILE | sed -r "s#$PLGIN_SRC_DIR#$INSTALLEDDIR#")
-      echo "${INSTALL_FILE}"
-      #yes | cp -f  $FILE $INSTALL_FILE
-      #ls -lad "${INSTALL_FILE}"
+      #echo "${INSTALL_FILE}"
+      yes | cp -f  $FILE $INSTALL_FILE
+      ls -lad "${INSTALL_FILE}"
     done
   fi
 done
